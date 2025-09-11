2f842d7172806677d8e5586dc3f758661604a917
Runtem <mnonomofficial28@gmail.com>
2025-09-11 11:10:09 +0700

    uhh build yeah

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ export function Card({ children, className = "" }) {
     );
 }
 
+// vercelllllllyljhtykkjotykjytpkjoptyj
+
 export function CardContent({ children, className = "" }) {
     return <div className={`p-4 ${className}`}>{children}</div>;
 }
